b0f892d842922d94456c7cf4f8d3a5a6566990c0
Ahmed <ahmedcodes48@gmail.com>
2025-01-16 23:42:19 +0500

    let code

---
+++ advanced/3- variables (let, const, var)/let.js
@@ -0,0 +1,11 @@
+let username = "Ahmed";
+console.log(username);
+
+username = "Mustafa";
+console.log(username);
+
+function call() {
+  let inCall = "calling you from inside function";
+}
+
+console.log(inCall);

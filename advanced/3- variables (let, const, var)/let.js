7ec6d551b4c98758ec559f7c752c9b67bcbf9847
Ahmed <ahmedcodes48@gmail.com>
2025-01-17 00:54:33 +0500

    let brief

--- advanced/3- variables (let, const, var)/let.js
+++ advanced/3- variables (let, const, var)/let.js
@@ -1,11 +1,31 @@
+//  ======================================= BLOCK SCOPE
+
+function scope() {
+  let inScope = "I m calling from inside the function";
+  console.log(
+    inScope
+  ); /* if i call function then inScope logged in console because it is in scope */
+}
+let inScope = "calling from outside";
+console.log(inScope); /* OUTPUT: function inScope is not defined */
+
+// ======================================== CANNOT REDECLARE
+
 let username = "Ahmed";
 console.log(username);
 
-username = "Mustafa";
-console.log(username);
+// let username = "REDECLARE AHMED";
+// console.log(username);                   OUTPUT: Identifier 'username' has already been declared
 
-function call() {
-  let inCall = "calling you from inside function";
-}
+// username = "Mustafa";
+// console.log(username);
 
-console.log(inCall);
+// function call() {
+//   let inCall = "calling you from inside function";
+//   return function () {
+//     console.log(inCall);
+//   };
+// }
+
+// let ahm = call();
+// ahm();

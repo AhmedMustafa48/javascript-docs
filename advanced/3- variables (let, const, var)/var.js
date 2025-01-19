468d444543c16b8b58abbef30a785b7506424893
Ahmed <ahmedcodes48@gmail.com>
2025-01-19 21:16:50 +0500

    var code

--- advanced/3- variables (let, const, var)/var.js
+++ advanced/3- variables (let, const, var)/var.js
@@ -8,3 +8,9 @@ console.log(x); // Output: 10 (not block-scoped)
 var name = "Alice";
 name = "Bob"; // Reassignment
 console.log(name); // Output: "Bob"
+
+// =========================== REDECLARATION
+
+var age = 25;
+var age = 30; // Redeclaration is allowed
+console.log(age); // Output: 30

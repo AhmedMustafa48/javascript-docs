3b619d428993fbd155d9d28205bc33796c694913
Ahmed <ahmedcodes48@gmail.com>
2025-01-19 21:17:24 +0500

    var code

--- advanced/3- variables (let, const, var)/var.js
+++ advanced/3- variables (let, const, var)/var.js
@@ -14,3 +14,9 @@ console.log(name); // Output: "Bob"
 var age = 25;
 var age = 30; // Redeclaration is allowed
 console.log(age); // Output: 30
+
+// =========================== HOISTING
+
+console.log(greeting); // Output: undefined (hoisted, but not initialized)
+var greeting = "Hello, world!";
+console.log(greeting); // Output: "Hello, world!"

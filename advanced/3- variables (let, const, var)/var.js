60500be1a3005bf1691c808a9a8016bfb9c53c3d
Ahmed <ahmedcodes48@gmail.com>
2025-01-19 21:16:08 +0500

    var code

--- advanced/3- variables (let, const, var)/var.js
+++ advanced/3- variables (let, const, var)/var.js
@@ -2,3 +2,9 @@ if (true) {
   var x = 10;
 }
 console.log(x); // Output: 10 (not block-scoped)
+
+// =========================== REASSIGNMENT
+
+var name = "Alice";
+name = "Bob"; // Reassignment
+console.log(name); // Output: "Bob"

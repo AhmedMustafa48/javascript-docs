62da1e699907862a7176433ef66bc442a3eed8f6
Ahmed <ahmedcodes48@gmail.com>
2025-01-20 23:26:25 +0500

    code line by line start

---
+++ advanced/4- code (line by line)/code.js
@@ -0,0 +1,7 @@
+var firstName = "Ahmed";
+let lastName = "Mustafa";
+let age = 15;
+const yearOfBirth = 2000;
+
+let userIntro = "Hi my name is " + firstName + " " + lastName;
+console.log(userIntro);

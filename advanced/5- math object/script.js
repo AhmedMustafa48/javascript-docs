07ec37ba67206d9ffd4b141e2496f9634767615f
Ahmed <ahmedcodes48@gmail.com>
2025-01-22 01:05:43 +0500

    more methods

---
+++ advanced/5- math object/script.js
@@ -0,0 +1,16 @@
+// ===================================== Mathematical Operator
+console.log("Addition is ", 15 + 4);
+console.log("SUbtraction is ", 15 - 4);
+console.log("Multiplication is ", 15 * 4);
+console.log("Division is ", 15 / 4);
+console.log("Remainder/Module is ", 15 % 4);
+console.log("Square  is ", 2 ** 2);
+
+// ===================================== Math Object Methods
+console.log(Math.floor(2.87)) /* 2 */
+console.log(Math.ceil(2.87)) /* 3 */
+console.log(Math.round(3)) /* 3 */
+console.log(Math.random() * 5 ) /* random decimal number range b/w 0-5 */
+
+
+

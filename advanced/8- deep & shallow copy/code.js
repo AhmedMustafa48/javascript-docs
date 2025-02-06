3508bcf271996dbb535438860c16fe97323a7e1c
Ahmed <ahmedcodes48@gmail.com>
2025-02-06 23:52:51 +0500

    deep shallow , final

--- advanced/8- deep & shallow copy/code.js
+++ advanced/8- deep & shallow copy/code.js
@@ -10,3 +10,8 @@ console.log(myFruits);
 console.log(fruits); 
 
 // Both arrays changed it happened because array is mutable same with objects.
+
+
+// Final Takeaway
+// Shallow copy = Copy top-level properties, but nested objects share references.
+// Deep copy = Copy everything, including nested properties, so changes don’t affect the original.

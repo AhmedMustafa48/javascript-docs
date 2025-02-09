9f26bf40ec8141bda28df977e65c0b6793304ccf
Ahmed <ahmedcodes48@gmail.com>
2025-02-09 23:45:31 +0500

    changed

--- advanced/8- deep & shallow copy/code.js
+++ advanced/8- deep & shallow copy/code.js
@@ -4,6 +4,9 @@ const fruits  =["Mango" , "Apple" , "Orange"]
 
 const myFruits = fruits;
 
+myFruits.push("Banana");
+console.log(myFruits);
+console.log(fruits); 
 
 
 // Both arrays changed it happened because array is mutable same with objects.
